f1864008a182442ee2a012966a68d1617cd572ca
Samuel Lawrentz <samuel.lawerence@plivo.com>
2022-08-25 19:32:59 +0530

    Added build folder

---
+++ build/README.md
@@ -0,0 +1,60 @@
+# rc-json-editor
+A simple react component that handles your JSON editing woes in a wink 😉
+
+## The What and Why
+This is a simple React Component _(and hence the "rc")_ that takes your JSON as input and renders a beautiful UI which allows you to edit the JSON on the go. I searched the web for this component for my specific needs and was not able to find one. And so, I created one.
+
+### Why do you want a json-editor component?
+You may think, why waste time on a component, while you can put `content-editable="true"` on a `code` tag and allow your users to edit their JSON themselves. But the catch here is, not everyone knows the syntax of a JSON. This tool could help in that case.
+
+### This is not the "Perfect" component
+It is just a start, issues and PRs are welcome if you find this component helping your needs. Find the [Demo Storybook](https://samuellawrentz.github.io/rc-json-editor/?path=/story/introduction--page)
+
+## The How?
+This component purely works on a custom Datatype called `JsonArray` which extends the `Array` class. So you know, it is an array of sorts, with some additional custom components.
+
+This is a in-progress doc, so please visit the `JsonArrayClass.ts` file for the complete implementation.
+
+> Changes are welcome so are feedbacks.
+
+## Dependencies
+There are no big dependencies per se.
+- react
+- react-dom
+
+## Things to do
+- Provide the full control of the rendering part as a prop
+- Provide event callbacks
+
+## Usage
+
+Install the package from npm
+
+```bash
+npm install rc-json-editor
+```
+
+This is a simple usage example
+
+```jsx
+import { TreeWrapper } from 'rc-json-editor';
+
+<TreeWrapper
+  data={{
+    order: {
+      id: 'order_124831sf23j123',
+      isSuccess: true,
+      price: 70000
+    }
+  }}
+  onChange={() => {}}
+/>
+```
+
+## Props
+- `data` - A JSON that needs to be viewed/edited
+- `onChange` - A callback that gets executed when the `data` is updated
+
+
+#### Storybook
+Find the demo here - https://samuellawrentz.github.io/react-json-editor/?path=/story/introduction--page
---
+++ build/index.es.js
@@ -0,0 +1,189 @@
+import React from 'react';
+
+class JsonArray extends Array {
+    constructor(json, path = "", stateUpdater) {
+        super();
+        if (typeof json !== "object")
+            return;
+        this.path = path;
+        this._updateTree = () => stateUpdater({ value: this });
+        this._updateArray(json);
+    }
+    // util Method
+    static convertToJSON(treeData) {
+        return treeData.reduce((acc, data) => {
+            if (data.type === "object")
+                // Recursive call, if type is object
+                acc[data.key] = JsonArray.convertToJSON(data.value);
+            else
+                acc[data.key] = data.value;
+            return acc;
+        }, {});
+    }
+    // Inserts a node at the given index
+    addNode(idx, json) {
+        this._updateArray(json, idx);
+        this._updateTree();
+        return this;
+    }
+    // Inserts a sub-node at the given index
+    addSubNode(idx, json) {
+        if (this[idx].type === "object")
+            return this[idx].value.addNode(0, json);
+        this._updateArray(json, idx, true);
+        this._updateTree();
+        return this;
+    }
+    deleteNode(idx) {
+        this.splice(idx, 1);
+        this._updateTree();
+        return this;
+    }
+    // Update individual node
+    updateNode(index, key, value) {
+        try {
+            if (key !== undefined)
+                this[index].key = key;
+            if (value !== undefined)
+                this[index].value = value;
+        }
+        catch (e) {
+            console.log(e);
+        }
+        this._updateTree(this);
+        return this;
+    }
+    // Factory method that creates a node
+    // #private method
+    _createNode(key, json) {
+        const isObject = typeof json[key] === "object";
+        const xPath = `${this.path}[${this.length}]`;
+        return {
+            key,
+            type: typeof json[key],
+            path: xPath,
+            value: isObject
+                ? // Recursion if object
+                    new JsonArray(json[key], xPath + ".value", this._updateTree)
+                : json[key],
+        };
+    }
+    // Internal method that is used to update the array based on the propd
+    // #private method
+    _updateArray(json, idx, subNode) {
+        Object.keys(json).forEach((key) => {
+            const prop = this._createNode(key, json);
+            // If index is present, insert at index
+            // Or push at the end
+            if (subNode) {
+                this[idx].type = "object";
+                this[idx].value = new JsonArray(json, this[idx].path + ".value", this._updateTree);
+            }
+            else if (idx)
+                this.splice(idx, 0, prop);
+            else
+                this.push(prop);
+        });
+    }
+}
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+
+function __rest(s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+}
+
+const Tree = (_a) => {
+    var { data } = _a, props = __rest(_a, ["data"]);
+    return (React.createElement("div", { className: "group" }, data.map((item, idx) => {
+        return (React.createElement("div", { key: item.path, className: "child node-row" },
+            React.createElement("span", { onClick: (e) => {
+                    data.addSubNode(idx, { key: "value" });
+                } }, "➕"),
+            React.createElement("input", { value: item.key, onChange: (e) => {
+                    data.updateNode(idx, e.target.value);
+                } }),
+            React.createElement("span", { className: "spacer" }),
+            item.type === "object" ? (React.createElement(React.Fragment, null,
+                React.createElement("input", { value: "Object", disabled: true }),
+                React.createElement("span", { onClick: () => data.deleteNode(idx) }, "\uD83D\uDDD1"),
+                React.createElement(Tree, Object.assign({ data: item.value }, props)))) : (React.createElement(React.Fragment, null,
+                React.createElement("input", { value: item.value, onChange: (e) => {
+                        data.updateNode(idx, undefined, e.target.value);
+                    } }),
+                React.createElement("span", { onClick: () => data.deleteNode(idx) }, "\uD83D\uDDD1")))));
+    })));
+};
+
+function styleInject(css, ref) {
+  if ( ref === void 0 ) ref = {};
+  var insertAt = ref.insertAt;
+
+  if (!css || typeof document === 'undefined') { return; }
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var css_248z = ".react-json-tree .group .group {\n  margin: 0 12px;\n}\n.react-json-tree .group .child {\n  margin: 8px 0;\n}\n.react-json-tree .group span {\n  cursor: pointer;\n  margin: 0 8px;\n  display: inline-grid;\n  height: 28px;\n  place-items: center;\n  line-height: 28px;\n}\n.react-json-tree .group input {\n  height: 24px;\n  margin: 0;\n  font-size: 16px;\n  padding: 0 4px;\n}";
+styleInject(css_248z);
+
+class TreeWrapper extends React.Component {
+    constructor(props) {
+        super(props);
+        this.jsonTree = new JsonArray(props.data, "", this.setState.bind(this));
+        this.state = {
+            value: [],
+        };
+    }
+    componentDidUpdate(_, pState) {
+        var _a, _b;
+        if (this.state !== pState)
+            (_b = (_a = this.props).onChange) === null || _b === void 0 ? void 0 : _b.call(_a, this.state.value);
+    }
+    render() {
+        return (React.createElement("div", { className: "react-json-tree" },
+            React.createElement(Tree, { data: this.jsonTree })));
+    }
+}
+
+export { TreeWrapper };
+//# sourceMappingURL=index.es.js.map
